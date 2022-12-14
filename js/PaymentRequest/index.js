b3ae50f9ad508e126b929d74d7bd5a317f73f4a4
Tyler Coffman <tyler.coffman@appfolio.com>
2022-12-14 10:24:02 -0800

    Stop using DeviceEventEmitter.removeSubscription.
    removeSubscription was removed in react-native 0.70
    
    ps-id: b7d9c819-4942-4ace-b590-b34914409052

--- js/PaymentRequest/index.js
+++ js/PaymentRequest/index.js
@@ -435,19 +435,13 @@ export default class PaymentRequest {
 
   _removeEventListeners() {
     // Internal Events
-    DeviceEventEmitter.removeSubscription(this._userDismissSubscription);
-    DeviceEventEmitter.removeSubscription(this._userAcceptSubscription);
+    this._userDismissSubscription.remove();
+    this._userAcceptSubscription.remove();
 
     if (IS_IOS) {
-      DeviceEventEmitter.removeSubscription(
-        this._shippingAddressChangeSubscription
-      );
-      DeviceEventEmitter.removeSubscription(
-        this._shippingOptionChangeSubscription
-      );
-      DeviceEventEmitter.removeSubscription(
-        this._paymentMethodChangeSubscription
-      );
+      this._shippingAddressChangeSubscription.remove();
+      this._shippingOptionChangeSubscription.remove();
+      this._paymentMethodChangeSubscription.remove();
     }
   }
 
